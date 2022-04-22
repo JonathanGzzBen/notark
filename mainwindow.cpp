5d3b8418bdb0979f3c34388151f285cff216933c
JonathanGzzBen <jonathangzzben@gmail.com>
2022-04-22 17:20:10 -0500

    Add basic layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,18 @@
 #include "mainwindow.h"
+
+#include <QDockWidget>
+
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  this->setCentralWidget(ui->plainTextEdit);
+  QDockWidget *dockWidget = new QDockWidget(tr("Dock Widget"), this);
+  dockWidget->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+  dockWidget->setWidget(ui->fileExplorerTreeWidget);
+  dockWidget->setWindowTitle("File Explorer");
+  addDockWidget(Qt::LeftDockWidgetArea, dockWidget);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
