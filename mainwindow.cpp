332d9abac271083822883bd3e188964dfa4316f2
JonathanGzzBen <jonathangzzben@gmail.com>
2022-05-19 17:09:42 -0500

    Replace QTreeWidget with QTreeView with QFileSystemModel

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,31 @@
 #include "mainwindow.h"
 
 #include <QDockWidget>
+#include <QFileSystemModel>
+#include <QTreeView>
 
+#include "qheaderview.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
   this->setCentralWidget(ui->plainTextEdit);
-  QDockWidget *dockWidget = new QDockWidget(tr("Dock Widget"), this);
-  dockWidget->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-  dockWidget->setFeatures(QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetFloatable);
-  dockWidget->setWidget(ui->fileExplorerTreeWidget);
-  dockWidget->setWindowTitle("File Explorer");
-  addDockWidget(Qt::LeftDockWidgetArea, dockWidget);
+
+  auto fileSystemModel = new QFileSystemModel;
+  fileSystemModel->setRootPath(QDir::currentPath());
+  auto fileSystemTree = new QTreeView(this);
+  fileSystemTree->setModel(fileSystemModel);
+  for(int i{1}; i < fileSystemTree->header()->count(); i++) {
+    fileSystemTree->header()->hideSection(i);
+  }
+
+  auto fileExplorerDockWidget = new QDockWidget(this);
+  fileExplorerDockWidget->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+  fileExplorerDockWidget->setFeatures(QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetFloatable);
+  fileExplorerDockWidget->setWidget(fileSystemTree);
+  fileExplorerDockWidget->setWindowTitle("File Explorer");
+  addDockWidget(Qt::LeftDockWidgetArea, fileExplorerDockWidget);
 }
 
 MainWindow::~MainWindow() { delete ui; }
